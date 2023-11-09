d4cefe1e75c446a4444e0c5314981a83a893bd2e
Mr Khing Empire <kingsleybotchway11@localhost.localdomain>
2023-11-09 05:13:08 +0000

    Changed Implementation: configuration object will be passed as a parameter to connector strings functions

--- utils/connectors.js
+++ utils/connectors.js
@@ -1,14 +1,12 @@
 /*Create connetion strings to connect to mongo db storage and redis storage */
 import configuration from "config"
 
-const mongodbConfig = configuration.get("mongodbConfig")
-const redisConfig = configuration.get("redisConfig")
 const test = configuration.get("test")
 /**
  * mongodbConnector -> creates a connection string base on config to connect to the database
  * returns {String}: mongodb connction string
  */
-export let mongodbConnector = () => {
+export let mongodbConnector = (mongodbConfig) => {
     let document = test ? mongodbConfig.documentTest : mongodbConfig.documentDeploy
     let connectionString = "";
     if(mongodbConfig.password || mongodbConfig.username) {
@@ -22,7 +20,7 @@ export let mongodbConnector = () => {
 /**
  * redisConnector => creates connection string base on configuration to connect to the database
  */
-export let redisConnector= () => {
+export let redisConnector= (redisConfig) => {
     let connectionString = "";
     if(redisConfig.password || redisConfig.username) {
         connectionString = `redis://${redisConfig.username}:${redisConfig.password}@${redisConfig.host}/${redisConfig.dbNumber}`
