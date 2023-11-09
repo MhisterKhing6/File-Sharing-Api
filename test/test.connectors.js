0e0ba627f27e2ae5ad17aa165d6a1731695e01f8
Mr Khing Empire <kingsleybotchway11@localhost.localdomain>
2023-11-09 05:22:35 +0000

    chore: test for mongodb connector string function base on new implemntation

--- test/test.connectors.js
+++ test/test.connectors.js
@@ -1,17 +1,15 @@
 import {assert} from "chai";
 import { mongodbConnector } from "../utils/connectors.js";
-import configs from "config"
-import { skip } from "node:test";
 
 
 describe("validate mongodb connection string", () => {
-    
-    xit("checks if default connections string is valid", () => {
+    let mockedMonogdbTestConfig = {port: "27017", host:"localhost", documentTest:"testMediaHub", username: "", password:""}
+    it("checks if default connections string is valid", () => {
         /**
          * has to be skipped, other configs prevent the defulat configs
          */
         let defaultConnectionString = `mongodb://localhost:27017/testMediaHub`
-        assert.equal(defaultConnectionString, mongodbConnector())
+        assert.equal(defaultConnectionString, mongodbConnector(mockedMonogdbTestConfig))
     })
 
     it("expect changes in config varibles results in different connection strings",()=> {
@@ -20,8 +18,10 @@ describe("validate mongodb connection string", () => {
          * password: test,
          * username: test
          */
+        mockedMonogdbTestConfig.username = "test"
+        mockedMonogdbTestConfig.password = "test"
         let expectedConnectionString = `mongodb://test:test@localhost:27017/testMediaHub`
-        assert.equal(mongodbConnector(), expectedConnectionString)
+        assert.equal(mongodbConnector(mockedMonogdbTestConfig), expectedConnectionString)
     })
 
 })
