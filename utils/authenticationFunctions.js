aa729de7da2441bc1441148de9027ed824ce3dcf
Kingsley Botchway <kingsleybotchwayedu11@gmail.com>
2023-11-12 04:43:13 +0000

    chore: added token generation function

--- utils/authenticationFunctions.js
+++ utils/authenticationFunctions.js
@@ -1,4 +1,5 @@
-import sha1 from "sha1"
+import sha1 from "sha1";
+import {v4} from "uuid"
 /**
  * Funcitons for authentication
  *   
@@ -14,3 +15,10 @@ export function encryptPassword(password) {
     return sha1(password)
 }
 
+/**
+ * generateToken : generate a token for a user
+ * returns {string}: generated token
+ */
+export function generateToken() {
+    return v4()
+}
