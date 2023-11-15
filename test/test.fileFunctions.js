2b45bfeea94f986a7a26ac62b9877eb6a1f21615
Kingsley Botchway <kingsleybotchwayedu11@gmail.com>
2023-11-15 09:36:17 +0000

    chore: test for genrating downloadable url

--- test/test.fileFunctions.js
+++ test/test.fileFunctions.js
@@ -1,14 +1,27 @@
 import {assert} from "chai";
-import {getFileUrl} from "../utils/fileFunctions.js"
+import {getFileUrl, generateDownloadUrl} from "../utils/fileFunctions.js"
 import configs from "config"
 import { test } from "node:test";
+let serverConfig = configs.get("expressConfig")
+
 
 describe("get url of a file", function () {
-    let serverConfig = configs.get("expressConfig")
     it("should return url of file", function(){
         let filePath = "storage/file/file2.txt"
         let url = getFileUrl(filePath)
         let expectedUrl = `http://${serverConfig.host}:${serverConfig.port}/${filePath}`
         assert.equal(url, expectedUrl)
+    })
+})
+
+
+describe("get downloadble url of a file", function() {
+    it("should downloadable url of the file", function() {
+        let fileId = "12345"
+        let token = "34567"
+        let expectedUrl = `http://${serverConfig.host}:${serverConfig.port}/downloadfile/${token}/${fileId}`
+        let downloadUrl = generateDownloadUrl(fileId, token)
+        assert.equal(expectedUrl, downloadUrl)
+        
     })
 })
